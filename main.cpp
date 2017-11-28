a4f68c48db91267344afaeb7f5888a8fe9a4d875
Tomáš Blažek <tblazek33@gmail.com>
2017-11-28 23:20:13 +0100

    Alpha 1.0 ..funkčnost, Dodělat výpisy

--- main.cpp
+++ main.cpp
@@ -9,70 +9,202 @@
 
 #define CAPACITY 40
 
-#define RUNTIME 1  //hours
+#define RUNTIME 40  //hours
 
 
 //Globals
 double generatePeople = GENERATE_PEOPLE;
 double countOfStarts =  COUNT_OF_STARTS;
-
+Entity *boat;
 
 //TODO dočasné
-int count = 0;
+int count1 = 0;
+int count2 = 0;
 
 
-//Stores Boat, Pier
-Store Boat("Boat capacity", CAPACITY);
-Store Pier1("Pier-Podhori", 1);
-Store Pier2("Pier-Podbaba", 1);
+//Stores Boat_place, Pier
+Store Boat_place("Boat_place capacity", CAPACITY);
+Facility Pier1("Pier-Podhori");
+Facility Pier2("Pier-Podbaba");
+Facility Boat("Boat");
 
-//Processes Boat, Passager, Time
+//Processes Boat_place, Passager1, Time
 
-class WorkTime : public Process{
+Queue queue1;
+Queue queue2;
+Queue queueLoaded;
+
+
+class BoatRide : public Process{
     void Behavior(){
-        printf("Call boat %f\n", Time);
-        DayTime->Activate;
+        Wait(Uniform(0.016666667,0.033333333));
     }
 };
-
-
 
 class DayTime : public Process{
-    void Behavior(){
-        for(int i = 0; i < countOfStarts; i++){
-            (new WorkTime)->Activate(Time+(15/60));
+
+    void Behavior() {
+        Seize(Pier1);
+        Seize(Pier2);
+        double timeBefore;
+        double waitingTime;
+        while(true){
+            for(int i = 0; i < countOfStarts; i++){
+                printf("-------Ready in Pier1-------\n");
+                Release(Pier1);
+                if(!queueLoaded.Empty()){
+                    (queueLoaded.GetFirst())->Activate();
+                }
+                timeBefore = Time;
+                WaitUntil(Boat_place.Empty());
+                waitingTime = Time - timeBefore;
+
+                if(!queue1.Empty()){
+                    queue1.GetFirst()->Activate();
+                }
+                Wait(0.25-waitingTime);
+                Seize(Pier1);
+                printf("BoatRide1 (%f)\n\n", Time);
+                (new BoatRide)->Activate();
+                printf("-------Ready in Pier2-------\n");
+                Release(Pier2);
+                if(!queueLoaded.Empty()){
+                    (queueLoaded.GetFirst())->Activate();
+                }
+                timeBefore = Time;
+                WaitUntil(Boat_place.Empty());
+                waitingTime = Time - timeBefore;
+
+                if(!queue2.Empty()){
+                    queue2.GetFirst()->Activate();
+                }
+
+                Wait(0.066666667-waitingTime);
+                Seize(Pier2);
+                printf("BoatRide2(back) (%f)\n\n", Time);
+                (new BoatRide)->Activate();
+
+            }
+            printf("-------Ready in Pier1-------\n");
+            Release(Pier1);
+            if(!queueLoaded.Empty()){
+                (queueLoaded.GetFirst())->Activate();
+            }
+            timeBefore = Time;
+            WaitUntil(Boat_place.Empty());
+            waitingTime = Time - timeBefore;
+            Seize(Pier1);
+            Wait(7-waitingTime);
+        }
+    }
+};
+
+
+
+class Passager1 : public Process{
+    void Behavior() {
+        int id = count1;
+        printf("%d. Passager1 (%f)\n", count1++, Time);
+
+
+        if(!queue1.Empty()){
+            printf("Passeger1(%d) going to queue1\n",id);
+            queue1.Insert(this);
             Passivate();
-            //call activate boat
-            printf("Call boat %f\n", Time);
         }
 
-        Activate(Time+(7));
-        printf("Call boat %f\n", Time);
+        repeatBoard:
+        if(!Boat_place.Full() && !Pier1.Busy()) {
+            Seize(Pier1);
+        }else{
+            printf("Passeger1(%d) going to queue1\n",id);
+            queue1.Insert(this);
+            Passivate();
+            goto repeatBoard;
+        }
+        double uniform = Uniform(0.000277778,0.000833333);
+        Wait(uniform);
+        Enter(Boat_place);
+        printf("Board passager1(%d) in Pier1 (%f)\n",id ,Time+uniform);
+        Release(Pier1);
 
-        //call activate boat
+        if(!queue1.Empty()){
+            (queue1.GetFirst())->Activate();
+        }
+
+        queueLoaded.Insert(this);
+        Passivate();
+
+        Seize(Pier2);
+        uniform = Uniform(0.000277778,0.000833333);
+        Wait(uniform);
+        Leave(Boat_place);
+        printf("Unboard1 passager1(%d) in time (%f)\n", id , Time + uniform);
+        Release(Pier2);
+        if(!queueLoaded.Empty()){
+            (queueLoaded.GetFirst())->Activate();
+        }
+
+        //výstup
+        //Cesta
     }
 };
 
+class Passager2 : public Process{
+    void Behavior() {
+        int id = count2;
+        printf("%d. Passager2 (%f)\n", count2++, Time);
 
-class Boat : public Process{
-    void Behavior(){
+        if(!queue2.Empty()){
+            printf("Passeger2(%d) going to queue2\n",id);
+            queue2.Insert(this);
+            Passivate();
+        }
 
+        repeatBoard:
+        if(!Boat_place.Full() && !Pier2.Busy()) {
+            Seize(Pier2);
+        }else{
+            printf("Passeger2(%d) going to queue2\n",id);
+            queue2.Insert(this);
+            Passivate();
+            goto repeatBoard;
+        }
+        double uniform = Uniform(0.000277778,0.000833333);
+        Wait(uniform);
+        Enter(Boat_place);
+        printf("Board passager2(%d) in Pier2 (%f)\n", id, Time + uniform);
+        Release(Pier2);
+
+        if(!queue2.Empty()){
+            (queue2.GetFirst())->Activate();
+        }
+
+        queueLoaded.Insert(this);
+        Passivate();
+
+        Seize(Pier1);
+        uniform = Uniform(0.000277778,0.000833333);
+        Wait(uniform);
+        Leave(Boat_place);
+        printf("Unboard2 passager2(%d) in time (%f)\n", id, Time+uniform);
+        Release(Pier1);
+        if(!queueLoaded.Empty()){
+            (queueLoaded.GetFirst())->Activate();
+        }
+
+        //výstup
+        //Cesta
     }
 };
-
-class Passager : public Process{
-    void Behavior(){
-            printf("%d. Passager (%f)\n", count++ , Time);
-
-    }
-};
-
 
 
 class GeneratorOfHumans : public Event {
 
     void Behavior(){
-        (new Passager)->Activate();
+        (new Passager1)->Activate();
+        Activate(Time + Exponential(generatePeople));
+        (new Passager2)->Activate();
         Activate(Time + Exponential(generatePeople));
     }
 };
@@ -86,7 +218,7 @@ int main() {
 
     //TODO parse aguments
 
-    Boat.SetCapacity(capacity);
+    Boat_place.SetCapacity(capacity);
 
     //SET PARAMETERS
     SetOutput("ims_output.out");
@@ -103,7 +235,6 @@ int main() {
     Run();
 
     //OUTPUTS
-    std::cout << "Hello, World!" << std::endl;
 
 
     return 0;
